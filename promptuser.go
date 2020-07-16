2b197403ffdcbbb447babd4903e50103f1c5bef7
mwyrick <mjw@axiosengineering.com>
2020-07-16 11:37:49 -0400

    Init

---
+++ promptuser.go
@@ -0,0 +1,52 @@
+package promptuser
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// Echo will prompt the user for input and echo there input
+func Echo(prompt string) string {
+	return base(prompt, true)
+}
+
+// NoEcho will prompt the user and hide there input
+func NoEcho(prompt string) string {
+	return base(prompt, false)
+}
+
+func base(prompt string, echo bool) string {
+	initialTermState, e1 := terminal.GetState(syscall.Stdin)
+	if e1 != nil {
+		panic(e1)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	defer signal.Stop(c)
+	go func() {
+		<-c
+		terminal.Restore(syscall.Stdin, initialTermState)
+		os.Exit(1)
+	}()
+
+	fmt.Print(prompt)
+
+	if echo {
+		reader := bufio.NewReader(os.Stdin)
+		u, _, _ := reader.ReadLine()
+		return string(u)
+	} else {
+		p, err := terminal.ReadPassword(syscall.Stdin)
+		fmt.Println("")
+		if err != nil {
+			panic(err)
+		}
+		return string(p)
+	}
+}
